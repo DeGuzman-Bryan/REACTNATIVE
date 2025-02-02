a3ff14828aa1fd5cbfc98a85052a6a96dfa0a392
DeGuzman-Bryan <enderkidbryan@gmail.com>
2025-02-02 13:34:45 +0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,43 @@
+import { StyleSheet, Text, View } from 'react-native' 
+import React from 'react' 
+
+const App = () => { 
+  return ( 
+    <View style={mystyles.container}> 
+      <View style={mystyles.box1}> 
+      </View> 
+      <View style={mystyles.box2}>
+      </View>
+      <View style={mystyles.box3}>
+      </View>
+    </View> 
+  ) 
+} 
+
+export default App 
+
+const mystyles = StyleSheet.create({ 
+  container:{ 
+    flex:1, 
+    justifyContent: 'flex-end', 
+    alignItems:'center' 
+  }, 
+  box1:{ 
+    backgroundColor:'darkcyan', 
+    width:100, 
+    height:100 
+  },
+
+  box2:{ 
+    backgroundColor:'darkgoldenrod', 
+    width:100, 
+    height:100 
+  },
+
+  box3:{ 
+    backgroundColor:'green', 
+    width:100, 
+    height:100 
+  }
+  
+}) 
---
+++ DeGuzman_Exercise/Exercise1.js
@@ -0,0 +1,27 @@
+import { StyleSheet, Text, View } from 'react-native' 
+import React from 'react' 
+
+const App = () => { 
+  return ( 
+    <View style={mystyles.container}> 
+      <View style={mystyles.box1}> 
+      </View>
+    </View> 
+  ) 
+} 
+
+export default App 
+
+const mystyles = StyleSheet.create({ 
+  container:{ 
+    flex:1, 
+    justifyContent: 'flex-end', 
+    alignItems:'center' 
+  }, 
+  box1:{ 
+    backgroundColor:'darkcyan', 
+    width:100, 
+    height:100 
+  }
+  
+}) 
